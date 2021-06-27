54f0da33537c5e94e7f1f8f935fa8892ce75c658
edyribowo <eribowo9@gmail.com>
2021-06-27 22:47:28 +0700

    ubah sesuatu

--- application/logs/log-2021-04-09.php
+++
@@ -1,4 +0,0 @@
-<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
-
-ERROR - 2021-04-09 23:40:27 --> The filetype you are attempting to upload is not allowed.
-ERROR - 2021-04-09 23:42:05 --> The filetype you are attempting to upload is not allowed.
--- application/modules/platform/models/M_platform.php
+++
@@ -1,7 +0,0 @@
-<?php
-class M_platform extends CI_Model {
-	public function saverecords($data) {
-        $this->db->insert('platform_kejadian',$data);
-        return true;
-	}
-}
